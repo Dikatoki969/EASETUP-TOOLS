716140a96cbf29de2e4e94633ae581cfdbb21fac
Ahmad andhika <ahmadandhika969@gmail.com>
2026-02-16 13:23:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,9 @@
 echo "Selamat datang di installer Easetup!"
 echo "Update & Install dependency..."
 apt update -y && apt install curl git -y
-# Pastikan Python3 & pip terinstall
 apt update -y && apt install python3 python3-pip -y
-# Install module yang dibutuhkan
 pip3 install --upgrade pip
 pip3 install questionary psutil rich
-
-# Jalankan script utama
 curl -s https://raw.githubusercontent.com/Dikatoki969/EASETUP-TOOLS/refs/heads/main/start.py -o ~/start.py
-echo "Selesai! Jalankan dengan: python3 ~/start.py"
+echo "Selesai! Jalankan dengan: python ~/start.py"
 python ~/start.py
